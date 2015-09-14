5f52c8fa20106f5b3904f200bd56cc0d884bfd21
AlexFolland <lexlexlex@gmail.com>
2015-09-14 15:34:00 -0700

    initial commit

---
+++ CooldownReadyFlashDisabler.lua
@@ -0,0 +1,2 @@
+for k,v in pairs(_G) do if type(v)=="table" and type(v.SetDrawBling)=="function" then v:SetDrawBling(false) end end
+hooksecurefunc(getmetatable(ActionButton1Cooldown).__index, 'SetCooldown', function(self) self:SetDrawBling(false) end)
