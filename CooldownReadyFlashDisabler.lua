cd32e3c02874ca17792383ef9efcf0c951c0fb4b
AlexFolland <lexlexlex@gmail.com>
2016-08-20 10:16:26 -0700

    toc and readability update

--- CooldownReadyFlashDisabler.lua
+++ CooldownReadyFlashDisabler.lua
@@ -1,2 +1,8 @@
-for k,v in pairs(_G) do if type(v)=="table" and type(v.SetDrawBling)=="function" then v:SetDrawBling(false) end end
-hooksecurefunc(getmetatable(ActionButton1Cooldown).__index, 'SetCooldown', function(self) self:SetDrawBling(false) end)
+for k,v in pairs(_G) do
+    if type(v)=="table" and type(v.SetDrawBling)=="function" then
+        v:SetDrawBling(false)
+    end
+end
+hooksecurefunc(getmetatable(ActionButton1Cooldown).__index, 'SetCooldown', function(self)
+    self:SetDrawBling(false)
+end)
